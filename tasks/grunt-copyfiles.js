bc75029f8e1d498a9a99262a7b0fa9b9922c6c86
balmor <dduda@nexway.com>
2015-05-29 16:15:53 +0200

    Multiple files part 2

--- tasks/grunt-copyfiles.js
+++ tasks/grunt-copyfiles.js
@@ -30,45 +30,34 @@ module.exports = function (grunt) {
         compile  = 'compile';
 
     this.files.forEach(function(file) {
-      var args = [];
-      var livingProcess = {
-        cmd: command,
-        args: [compile, file.src]
-      };
-
       file.src.filter(function(filepath) {
+        var livingProcess = {
+          cmd: 'livingstyleguide',
+          args: ['compile', filepath]
+        };
+        function livingDone(error, result, code) {
+          if (result.stdout) {
+            grunt.log.ok(String(result));
+          }
+          if (error) {
+            return done(error);
+          } else {
+            return done();
+          }
+        }
+        grunt.log.writeln('\'' + command + ' ' + compile + ' ' + filepath + '\'');
+        grunt.util.spawn(livingProcess, livingDone);
+
         // Remove nonexistent files (it's up to you to filter or warn here).
         if (!grunt.file.exists(filepath)) {
           grunt.log.warn('Source file "' + filepath + '" not found.');
           return false;
         } else {
           console.log(filepath);
-          var n = 1;
-          n++
-          grunt.log.writeln(n);
           // console.log(dest);
           return true;
         }
-      }).map(function(filepath) {
-        // Read and return the file's source.
-        return grunt.file.read(filepath);
       });
-
-      function livingDone(error, result, code) {
-        if (result.stdout) {
-          grunt.log.ok(String(result));
-        }
-        if (error) {
-          return done(error);
-        } else {
-          return done();
-        }
-      }
-
-      grunt.log.writeln('\'' + command + ' ' + compile + ' ' + file.src + '\'');
-
-      // Run livingstyleguide
-      // grunt.util.spawn(livingProcess, livingDone);
     });
 
   });
